835b94bcf474029fc588187720769b8a472c63c5
Clément Turbelin <clement.turbelin@gmail.com>
2019-10-25 12:02:30 +0200

    add share/lib base bootstrap

---
+++ share/lib/system.R
@@ -0,0 +1,26 @@
+##
+# Bootstrap Loader of global configuration
+##
+
+options(ifn=.ifnOptions)
+
+if(!library(ifnBase, logical.return = TRUE)) {
+  if( !interactive() ) {
+    stop("Missing library ifnBase")
+  }
+}
+
+load_platform()
+
+if(!"methods" %in% loadedNamespaces()) {
+  # methods Lazy loading
+  is <- function(...) {
+
+    if(!isTRUE(ifnBase::get_option("methods.loaded"))) {
+      library(methods)
+      share.option(methods.loaded=TRUE)
+    }
+    methods::is(...)
+  }
+}
+
