cca806e568babca338298f4a73f455532621cd75
Clément Turbelin <clement.turbelin@gmail.com>
2019-11-19 20:23:13 +0100

    remove old import script

--- import/sql/import/post-import-old.sql
+++
@@ -1,9 +0,0 @@
-
-ALTER TABLE epidb_results_intake ADD COLUMN dump integer;
-
-UPDATE epidb_results_intake SET dump=2013;
-
-ALTER TABLE epidb_results_weekly ADD COLUMN dump integer;
-
-UPDATE epidb_results_weekly SET dump=2013;
-
