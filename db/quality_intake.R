5b7ed78beca48622415a31b8306cfbdcb2b53c38
Clément Turbelin <clement.turbelin@upmc.fr>
2019-11-25 15:16:40 +0100

    quality check for global id

--- db/quality_intake.R
+++ db/quality_intake.R
@@ -4,7 +4,9 @@ source('conf.R')
 
 library(dplyr)
 library(reshape2)
+library(ggplot2)
 
+init.path("quality")
 
 check_global_id = function(table) {
   r = dbQuery('select count(*) as total, sum(case when global_id is null or global_id=\'\' then 1 else 0 end) missing_gid, "country",  extract(year from timestamp) as "year" from epidb_results_',table,' group by "country", "year" ')
@@ -14,24 +16,14 @@ check_global_id = function(table) {
 r_intake = check_global_id("intake")
 r_weekly = check_global_id("weekly")
 
+rr = bind_rows(r_intake, r_weekly, .id = 'survey')
 
-ht = function(data, row, col, value) {
-  rr = by(data, data[, col, drop=FALSE], function(r) {
-    r[, c(row, value), drop=FALSE]
-  })
-  o = NULL
-  for(i in 1:length(rr)) {
-    d = rr[[i]]
-    names(d) <- c(row, names(rr[i]))
-    if(is.null(o)) {
-      o = d
-    } else {
-      o = merge(o, d, by=row, all=TRUE)
-    }
-  }
-  o
-}
+ggplot(rr, aes(x=factor(year), y=prop, fill=survey)) + 
+    geom_bar(stat="identity", position = "dodge") + 
+    facet_grid(rows=vars(country)) +
+    labs(y="% of missing global_id", x="Year", title="Missing global_id by country, year and survey") +
+    scale_fill_discrete(labels=c("1"="Intake","2"="Weekly")) 
+ggsave(my.path('missing_global_id.pdf'), width=8, height=15)
 
-ht(r_intake, "country","year", "lab")
+write.csv2(rr, my.path('missing_global_id.csv'), row.names = FALSE)
 
-ht(r_weekly, "country","year", "lab")
