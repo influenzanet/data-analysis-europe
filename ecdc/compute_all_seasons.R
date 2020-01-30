4e82ec8be76f132e60068c19df857bc170fe67d2
Clément Turbelin <clement.turbelin@gmail.com>
2020-01-30 12:48:11 +0100

    compute healthcare indicator

--- ecdc/compute_all_seasons.R
+++ ecdc/compute_all_seasons.R
@@ -1,23 +1,50 @@
 # Compute all
 source("conf.R")
 
-for(season in get_historical_seasons()) {
+if(!interactive()) {
+  library(swMisc)
+  
+  compute = parseArgs(list(
+    "incidence"=list(type="bool", default=TRUE),
+    "healthcare"=list(type="bool", default=TRUE)
+  ))
+} else {
+  if(!exists("compute")) {
+    stop("compute variable must be defined")
+  }
+}
+
+run_script = function(season, country, name) {
+  cat("-- Running ", name,"\n")
+  r = try({
+    source(paste0(name, ".R"), local=TRUE)
+  }, silent = TRUE)
+  
+  if(is(attr(r,"condition"), "error_no_data")) {
+    message("No data for this season")
+    return()
+  }
+  
+  if( is.error(r) ) {
+    warning("Error during computing")
+    print(r)
+  }
+  r
+}
+
+seasons = get_historical_seasons()
+for(season in seasons) {
   countries = platform_env("COUNTRY_CODES")
   for(country in countries) {
     cat("=== Computing ", country, " season ", season,"\n")
-    r = try(local({
-      source("ecdc_indicator.R", local=TRUE)
-    }), silent = TRUE)
     
-    if(is(attr(r,"condition"), "error_no_data")) {
-      message("No data for this season\n")
-      next()
+    if(isTRUE(compute$incidence)) {
+      run_script(season, country, "ecdc_indicator")
     }
     
-    if( is.error(r) ) {
-      warning("Error during computing")
-      print(r)
+    if(isTRUE(compute$episode)) {
+     run_script(season, country, "ecdc_healthcare")
     }
-    
+
   }
 }
