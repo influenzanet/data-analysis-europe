322a12a7d62aaa0507056d7636be14d11a3e9a1c
Clément Turbelin <clement.turbelin@gmail.com>
2020-02-03 15:15:34 +0100

    add country/compute_all

---
+++ country/compute_all.R
@@ -0,0 +1,29 @@
+# Compute all
+source("conf.R")
+
+run_script = function(season, country, name) {
+  cat("-- Running ", name,"\n")
+  r = try({
+    source(paste0(name, ".R"), local=TRUE)
+  }, silent = TRUE)
+  
+  if(is(attr(r,"condition"), "error_no_data")) {
+    message("No data for this season")
+    return()
+  }
+  
+  if( is.error(r) ) {
+    warning("Error during computing")
+    print(r)
+  }
+  r
+}
+
+seasons = get_historical_seasons()
+for(season in seasons) {
+  countries = platform_env("COUNTRY_CODES")
+  for(country in countries) {
+    cat("=== Computing ", country, " season ", season,"\n")
+    run_script(season, country, "intakes")
+  }
+}
