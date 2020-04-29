7a7d38e7a5a64ae15fb3cf01e2abf878fb18b590
Clément Turbelin <clement.turbelin@gmail.com>
2020-04-29 18:53:41 +0200

    add country name

--- share/platform/eu/i18n.en.R
+++ share/platform/eu/i18n.en.R
@@ -1,3 +1,14 @@
 
 color.web.primary = '#003556'
 color.web.secondary = '#01a69d'
+
+country_FR = 'France'
+country_DK = 'Danemark'
+country_UK = 'England'
+country_IE = 'Ireland'
+country_IT = 'Italy'
+country_PT = 'Portugal'
+country_ES = 'Spain'
+country_CH = 'Switzerland'
+country_BE = 'Belgium'
+country_NL = 'Netherlands'
