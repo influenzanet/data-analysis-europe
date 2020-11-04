38900825ca2e665f3682788dbd16ef13df8cc586
Clément Turbelin <clement.turbelin@gmail.com>
2020-11-04 18:11:15 +0100

    refactor bundle building, no more csv input

--- ecdc/build_bundle.R
+++ ecdc/build_bundle.R
@@ -58,8 +58,8 @@ load_incidence_country = function(country) {
 lapply(countries, load_incidence_country)
 
 datasets$active %<>% 
-                    mutate_at(c("syndrome", "country", "method"), factor) %>% 
-                    arrange(season, yw)
+    mutate_at(c("syndrome", "country", "method"), factor) %>% 
+    arrange(season, yw)
 
 datasets$inc %<>%
     mutate_at(c("syndrome", "country", "method"), factor) %>% 
@@ -69,110 +69,133 @@ saveRDS(datasets, my.path("incidences.rds"))
 
 dir.create(my.path('bundles'), showWarnings = FALSE)
 
-filters = list(
-  list()
+#' Remove first estimated point of the season and restrict range (for season < 2020)
+filter_season_weeks = function(data) {
   
-  
-  
-  
-)
-
-
-#' @param path path where are by seasons files
-#' @param name name of file to create
-#' @param sorting column to use to sort the data
-#' @param restrict.yw restrict the range of weeks according to the weeks of the season
-#' @param filter function() to filter data
-#' @param country
-create_bundle = function(path, name, country, sorting, restrict.yw=TRUE, filter=NULL, out.name=NULL) {
-  cat("Importing", name, country,"\n")
-  data = NULL
-  for(season in seasons) {
-    file = paste0(path, name, "_", season, ".csv")
+  restrict_season = function(r, .keys) {
+    season = .keys$season
     
-    if(!file.exists(file)){
-      next()
-    } 
-    
-    if(country == "IE" && season == 2018) {
-      next()
+    if(season >= 2020) {
+      return(r)
     }
     
-    if(country %in% c("BE", "NL") && season == 2017) {
-      next()
+    if(season == 2019) {
+      season.bounds = c(40, 30) # 2019 : include summer, no season end
+    } else {
+      season.bounds = c(40, 18) # Before 2019  week 40 to week 18 of y+1
     }
     
+    ## Remove 2 first estimation points
+    # Computed but given the computation rules they are not significants
+    ww = sort(unique(r$yw))
+    w = head(ww, n=2)
+    #cat("Excluding ", w,"\n")
+    r = r[ !r$yw %in% w, ]
     
-    r = read.csv2(file)
+    # Maximum season bounds
+    season.range = ((as.integer(season) + c(0, 1)) * 100) + season.bounds
     
-    if(restrict.yw) {
-      ## Remove 2 first estimation points
-      # Computed but given the computation rules they are not significants
-      ww = sort(unique(r$yw))
-      w = head(ww, n=2)
-      #cat("Excluding ", w,"\n")
-      r = r[ !r$yw %in% w, ]
-      
-      # Maximum season bounds
-      season.range = ((as.integer(season) + c(0, 1)) * 100) + c(40, 18)
-      
-      r = r[ r$yw >= season.range[1] & r$yw <= season.range[2], ]
-    }
-    
-    if(nrow(r) > 0 && is.null(r[["season"]])) {
-      r$season = as.integer(season)
-    }
-    
-    data = bind_rows(data, r)
+    r = r[ r$yw >= season.range[1] & r$yw <= season.range[2], ]
+    r
   }
-  
-  if(!is.null(data)) {
-    data = data %>% arrange(!!!syms(sorting))
-    
-    if(!is.null(filter)) {
-      data = do.call(filter, list(data))
-    }
-    if(is.null(out.name)) {
-      out.name = name
-    }
-    write.csv(data, file=my.path('bundles/', country, '_', out.name,'.csv'), row.names = FALSE)
-  }
-  
-  data
+  data %>% group_by(season) %>% group_modify(restrict_season)
 }
 
-
+#' Incidence data selection
 filter_incidence = function(data) {
-  i = is.na(data$upper) & is.na(data$lower)
-  data$upper[i] = 0
-  data$lower[i] = 0
-  data
+  
+  na_to_zero = function(x) {
+    x[is.na(x)] = 0
+    x
+  }
+  
+  data %>% 
+    mutate(across(c(upper, lower), na_to_zero )) %>%
+    filter(type == "adj") %>%
+    filter( (syndrome == "ili.ecdc" & method == "w1_s2_if2_ex") | (syndrome == "covid.ecdc" & method == "w0_s2_if2_ex") )
 }
+
 
 filter_visits = function(data) {
   data %>% filter(estimator == "adj")
 }
 
-for(country in countries) {
-  path = my.path(country,"/")
-  create_bundle(path, "active", country, "yw")
-  create_bundle(path, "incidence", country, c("yw","syndrome"), filter=filter_incidence )
-  create_bundle(path, "visits_weekly", country, c('yw','variable'), out.name="visits_weekly_all")
-  visits = create_bundle(path, "visits_weekly", country, c('yw','variable'), filter=filter_visits)
-  if(!is.null(visits)) {
-    last = visits %>% 
-              group_by(season) %>%
-              mutate(max_yw=max(yw)) %>%
-              ungroup() %>%
-              filter(yw == max_yw) %>% 
-              select(yw, variable, cum_prop, cum_prop_upper, cum_prop_lower, season)
-    
-    if(nrow(last) > 0) {
-      last$cum_prop_upper[!is.na(last$cum_prop_upper) & last$cum_prop_upper > 1] = 1
-      last$cum_prop_lower[!is.na(last$cum_prop_lower) & last$cum_prop_lower < 0] = 0
-    }
-    
-    write.csv(last, file=my.path('bundles/', country, '_visits_cumul.csv'), row.names = FALSE)
-  } 
+#' Remove non constistent data
+filter_base = function(data) {
+  data %>% filter(
+    !(country == "IE" & season == 2018),
+    !(country %in% c("BE","NL") & season == 2017)
+  )
   
 }
+
+# Bundles definition
+bundles = list(
+  list(
+    name="incidence", 
+    sorting=c("yw","syndrome"), 
+    dataset="inc", 
+    filters = list(
+      filter_base,
+      filter_season_weeks,
+      filter_incidence
+      )
+  ),
+  list(
+    name="active",
+    sorting="yw",
+    dataset="active",
+    filters= list(
+      filter_base,
+      filter_season_weeks
+    )
+  )
+)
+
+# Create a bundle for a country
+create_bundle_country = function(data, .keys, bundle) {
+  country = .keys$country
+  data = data %>% arrange(!!!syms(bundle$sorting))
+  message(paste("+  Writing bundle", country, nrow(data), "rows"))
+  write.csv(data, file=my.path('bundles/', country, '_', bundle$name,'.csv'), row.names = FALSE) 
+}
+
+# Build bundles
+for(bundle in bundles) {
+  name = bundle$name
+  message(paste("Bundle", name))
+  data = datasets[[bundle$dataset]]
+  if(is.null(data)) {
+    message(paste("No data for bundle", name))
+    next()
+  }
+  filters = bundle$filters
+  for(f in filters) {
+    data = do.call(f, list(data))
+  }
+  data %>% group_by(country) %>% group_walk(create_bundle_country, bundle=bundle)
+}
+
+# for(country in countries) {
+#   path = my.path(country,"/")
+#   create_bundle(path, "active", country, "yw")
+#   create_bundle(path, "incidence", country, c("yw","syndrome"), filter=filter_incidence )
+#   create_bundle(path, "visits_weekly", country, c('yw','variable'), out.name="visits_weekly_all")
+#   visits = create_bundle(path, "visits_weekly", country, c('yw','variable'), filter=filter_visits)
+#   if(!is.null(visits)) {
+#     last = visits %>% 
+#               group_by(season) %>%
+#               mutate(max_yw=max(yw)) %>%
+#               ungroup() %>%
+#               filter(yw == max_yw) %>% 
+#               select(yw, variable, cum_prop, cum_prop_upper, cum_prop_lower, season)
+#     
+#     if(nrow(last) > 0) {
+#       last$cum_prop_upper[!is.na(last$cum_prop_upper) & last$cum_prop_upper > 1] = 1
+#       last$cum_prop_lower[!is.na(last$cum_prop_lower) & last$cum_prop_lower < 0] = 0
+#     }
+#     
+#     write.csv(last, file=my.path('bundles/', country, '_visits_cumul.csv'), row.names = FALSE)
+#   } 
+#   
+# }
