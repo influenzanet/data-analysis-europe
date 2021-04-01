29e5302714d87f750d2abb013f999e3cd69c0ca3
Clément Turbelin <clement.turbelin@gmail.com>
2021-04-01 17:13:42 +0200

    add .Rworkspace file

--- system.R
+++ system.R
@@ -2,8 +2,25 @@
 
 .ifnOptions = list() # global config
 
+find_workspace = function(mark.file=".Rworkspace", max.depth=10) {
+  depth = 0
+  dir = getwd()
+  found = FALSE
+  while(!found && depth < max.depth) {
+    found = file.exists(file.path(dir, mark.file))
+    if(!found) {
+      dir = normalizePath(file.path(dir,".."))
+      depth = depth + 1
+    }
+  }
+  if(found) {
+    return(dir)
+  }
+  rlang::abort("Unable to find workspace")
+}
+
 # Local config scripts
-source('../location.R') # should always in the upper directory from working dir
+source(file.path(find_workspace(), 'location.R')) # should always in the upper directory from working dir
 
 if( is.null( getOption("ifn") ) ) {
   # load system bootstrap script
